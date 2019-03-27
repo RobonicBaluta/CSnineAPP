8939f22fa72760cedea2d506dbb7e275f2658af9
anderlab <anderlaboreo@gmail.com>
2019-03-27 12:45:52 +0100

    maybe this time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'new', loadChildren: './tab1/new/new.module#NewPageModule' },
-  { path: 'tab6', loadChildren: './tab6/tab6.module#Tab6PageModule' },
 
 ];
 @NgModule({
