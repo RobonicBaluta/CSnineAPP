cb48b55e16247f52e4cf2b65532f756e02a2dbfa
rbaluta <rbaluta@pc-web.at>
2019-04-02 14:17:29 +0200

    back to normal, authguard will be needed to make login default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  // { path: '', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'app', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'task-modal', loadChildren: './task-modal/task-modal.module#TaskModalPageModule' },
-  // { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'remember', loadChildren: './remember/remember.module#RememberPageModule' },
   { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
   { path: 'task-view-modal', loadChildren: './task-view-modal/task-view-modal.module#TaskViewModalPageModule' },
