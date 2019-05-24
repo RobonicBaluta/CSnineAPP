95057283dd912c26b74af362b65b4201ae0fd7f8
anderlab <anderlaboreo@gmail.com>
2019-05-24 17:29:12 +0200

    view mobile contact

--- src/app/modals/show-mobile-contact-modal/show-mobile-contact-modal.page.ts
+++ src/app/modals/show-mobile-contact-modal/show-mobile-contact-modal.page.ts
@@ -11,59 +11,72 @@ import { NavParams, AlertController } from '@ionic/angular';
 export class ShowMobileContactModalPage implements OnInit {
   contact:any;
   mobileContacId: any;
+  mobileContacts:any;
   constructor(private contacts: Contacts,
     private navParams:NavParams,
     public alertController: AlertController,
     ) { }
+    
+    ngOnInit() {
+      this.getContact();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    //   async getContacts() {
+    //     // const loading = await this.loadingController.create({
+    //     //     message: 'Loading'
+    //     // });
+    //     // await loading.present();
+    //     this.contacts.find(['id','displayName', 'name', 'phoneNumbers', 'emails'], {filter: "", multiple: true})
+    //     .then(data => {
+    //         this.mobileContacts = data
+    //         console.log(this.mobileContacts);
+    //     });
+    //     // loading.dismiss();
+    // }
+    
+    
+    
+    async getContact() {
+      await this.getId();
+      this.presentAlert(this.mobileContacId);
+      
+      this.contacts.find(['id'],{filter: `${this.mobileContacId}`, multiple: true}).then(data => {
+        this.mobileContacts = data
 
-  ngOnInit() {
-    this.getContact();
+        this.mobileContacts.forEach(cont => {
+          this.contact=cont;
+          this.presentAlert(this.contact.displayName);
+          this.presentAlert(this.contact.id);
+          this.presentAlert(this.contact.emails[0].value);
+          this.presentAlert(this.contact.phoneNumbers[0].value);
+          this.presentAlert(this.contact.addresses[0].value);
+        });
+        this.presentAlert('hey');
+      });
+      this.presentAlert(this.contact.displayName);
+      
+    }
+    
+    async getId(){
+      this.mobileContacId=this.navParams.get('mobileContactId');
+    }
+    
+    
+    async presentAlert(msg) {
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        message: msg,
+        buttons: ['OK']
+      });
+      
+      await alert.present();
+    }
   }
-
-
-
-
-
-
-
-
-//   async getContacts() {
-//     // const loading = await this.loadingController.create({
-//     //     message: 'Loading'
-//     // });
-//     // await loading.present();
-//     this.contacts.find(['id','displayName', 'name', 'phoneNumbers', 'emails'], {filter: "", multiple: true})
-//     .then(data => {
-//         this.mobileContacts = data
-//         console.log(this.mobileContacts);
-//     });
-//     // loading.dismiss();
-// }
-
-
-
-async getContact() {
-  this.mobileContacId=this.navParams.get('mobileContactId');
-  this.presentAlert(this.mobileContacId);
-
- await this.contacts.find(['id','displayName', 'name', 'phoneNumbers', 'emails'],{filter: `${this.mobileContacId}`, multiple: true}).then(data => {
-    this.contact = data
-    this.presentAlert(this.contact.displayName);
-});
-this.presentAlert(this.contact.displayName);
-
-}
-
-
-
-
-async presentAlert(msg) {
-  const alert = await this.alertController.create({
-    header: 'Alert',
-    message: msg,
-    buttons: ['OK']
-  });
   
-  await alert.present();
-}
-}
